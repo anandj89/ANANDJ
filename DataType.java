5ead626392081bef6c8670b8ef186f2cdd5b3109
anandj89 <104631393+anandj89@users.noreply.github.com>
2022-04-29 06:28:34 -0700

    FIRST PROJECT

---
+++ DataType.java
@@ -0,0 +1,36 @@
+
+public class DataType {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		/*
+		 * byte b=15; System.out.println("b is "+ b); System.out.println("byte size:"
+		 * +Byte.SIZE); System.out.println("min val:"+Byte.MIN_VALUE);
+		 * System.out.println("max val:"+Byte.MAX_VALUE);
+		 * 
+		 * int i=525; System.out.println("int size:" +Integer.SIZE);
+		 * System.out.println("min val:"+Integer.MIN_VALUE);
+		 * System.out.println("max val:"+Integer.MAX_VALUE);
+		 * 
+		 * float f=545645.545653f; System.out.println("byte size:" +Byte.SIZE);
+		 * System.out.println("min val:"+Float.MIN_VALUE);
+		 * System.out.println("max val:"+Float.MAX_VALUE);
+		 * 
+		 * double d=4545.45604d; System.out.println("double size:" +Byte.SIZE);
+		 * System.out.println("min val:"+Double.MIN_VALUE);
+		 * System.out.println("max val:"+Double.MAX_VALUE);
+		 * 
+		 * long l=9223372036854775807L; System.out.println("long size:" +Long.SIZE);
+		 * System.out.println("min val:"+Long.MIN_VALUE);
+		 * System.out.println("max val:"+Long.MAX_VALUE);
+		 */ 
+		  char c; System.out.println("min val:"+Character.MIN_VALUE);
+		  System.out.println("max val:"+(Character.MAX_VALUE));
+		 
+		
+		for(int index=0;index<345;index++) {
+			System.out.println(index+" The character is: "+ (char)index);
+		}
+	}
+
+}
---
+++ Greeting.java
@@ -0,0 +1,6 @@
+public class Greeting {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	}
+}
